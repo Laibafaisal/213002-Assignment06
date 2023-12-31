7a7fee7e2b1279e53d735d99c0a6363a707a06a1
Laiba Faisal <lfj1310@gmail.com>
2023-12-31 12:42:19 +0500

    Assign 6

---
+++ 213002-Assignment06/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _213002_Assignment06.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ 213002-Assignment06/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace _213002_Assignment06.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    
+}
